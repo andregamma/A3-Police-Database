65b2864c87a0cee14e962732342596e2eecf5f40
André Gama <31011359+andregamma@users.noreply.github.com>
2019-02-08 10:15:07 -0300

    Update db.php

--- php_includes/db.php
+++ php_includes/db.php
@@ -2,10 +2,11 @@
 //error_reporting(E_ALL);
 ini_set('display_errors',0);
 ini_set('display_startup_errors',0);
-define('HOST', "localhost");
-define('USER', "root");
-define('PASS', "");
-define('DB', "police");
+$dbopts = parse_url(getenv('DATABASE_URL'));
+define('HOST', $dbopts["host"]);
+define('USER', $dbopts["user"]);
+define('PASS', $dbopts["pass"]);
+define('DB', ltrim($dbopts["path"],'/'));
 
 function genPDO($DB = DB, $user = USER, $pass = PASS, $host = HOST) {
 	$pdo = new PDO("mysql:host=".$host.";dbname=".$DB, $user, $pass);
